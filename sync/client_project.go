87168c5d3d8ceed371b76829f59c36791787373d
CnTeng <rxsnakepi@gmail.com>
2025-04-03 23:24:13 +0800

    refactor(sync): standardize project api

--- sync/client_project.go
+++ sync/client_project.go
@@ -20,18 +20,15 @@ func (c *Client) MoveProject(ctx context.Context, args *ProjectMoveArgs) (*SyncR
 	return c.executeCommand(ctx, args)
 }
 
-func (c *Client) DeleteProject(ctx context.Context, id string) (*SyncResponse, error) {
-	args := &ProjectDeleteArgs{ID: id}
+func (c *Client) DeleteProject(ctx context.Context, args *ProjectDeleteArgs) (*SyncResponse, error) {
 	return c.executeCommand(ctx, args)
 }
 
-func (c *Client) ArchiveProject(ctx context.Context, id string) (*SyncResponse, error) {
-	args := &ProjectArchiveArgs{ID: id}
+func (c *Client) ArchiveProject(ctx context.Context, args *ProjectArchiveArgs) (*SyncResponse, error) {
 	return c.executeCommand(ctx, args)
 }
 
-func (c *Client) UnarchiveProject(ctx context.Context, id string) (*SyncResponse, error) {
-	args := &ProjectUnarchiveArgs{ID: id}
+func (c *Client) UnarchiveProject(ctx context.Context, args *ProjectUnarchiveArgs) (*SyncResponse, error) {
 	return c.executeCommand(ctx, args)
 }
 
@@ -43,8 +40,8 @@ func (c *Client) GetProjectInfo(ctx context.Context, params *ProjectGetInfoParam
 	return do[ProjectGetInfoParams, ProjectGetInfoResponse](ctx, c, projectsGetEndpoint, params)
 }
 
-func (c *Client) GetProjectData(ctx context.Context, id string) (*ProjectGetDataResponse, error) {
-	return do[ProjectGetDataParams, ProjectGetDataResponse](ctx, c, projectsGetDataEndpoint, &ProjectGetDataParams{ProjectID: id})
+func (c *Client) GetProjectData(ctx context.Context, params *ProjectGetDataParams) (*ProjectGetDataResponse, error) {
+	return do[ProjectGetDataParams, ProjectGetDataResponse](ctx, c, projectsGetDataEndpoint, params)
 }
 
 func (c *Client) GetArchivedProjects(ctx context.Context, params *ProjectGetArchivedParams) (*ProjectGetArchivedResponse, error) {
