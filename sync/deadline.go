8a8cb5e993b2ed444c52ffe7ed85e6c8763758a8
CnTeng <rxsnakepi@gmail.com>
2025-04-09 21:06:37 +0800

    chore(sync): make deadline fields optional

--- sync/deadline.go
+++ sync/deadline.go
@@ -15,14 +15,14 @@ import (
 // [Deadlines]: https://developer.todoist.com/sync/v9#deadlines
 type Deadline struct {
 	// Deadline in the format of YYYY-MM-DD (RFC 3339).
-	Date time.Time `json:"date"`
+	Date *time.Time `json:"date"`
 
 	// Lang which has to be used to parse the content of the string attribute.
 	// Used by clients and on the server side to properly process deadlines.
 	//
 	// Valid languages are:
 	//   en, da, pl, zh, ko, de, pt, ja, it, fr, sv, ru, es, nl, fi, nb, tw.
-	Lang string `json:"lang"`
+	Lang *string `json:"lang"`
 }
 
 func (d *Deadline) UnmarshalJSON(data []byte) error {
@@ -40,7 +40,7 @@ func (d *Deadline) UnmarshalJSON(data []byte) error {
 	if err != nil {
 		return err
 	}
-	d.Date = date
+	d.Date = &date
 
 	return nil
 }
