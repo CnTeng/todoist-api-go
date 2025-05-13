170a9fdfa6da13d6deb17cfcba39e291515507b3
CnTeng <rxsnakepi@gmail.com>
2025-05-13 16:27:12 +0800

    refactor(filter): add FilterService for filter api

--- todoist/filter.go
+++ todoist/filter.go
@@ -6,22 +6,31 @@ import (
 	"github.com/CnTeng/todoist-api-go/sync"
 )
 
-func (c *Client) AddFilter(ctx context.Context, args *sync.FilterAddArgs) (*sync.SyncResponse, error) {
-	return c.ExecuteCommand(ctx, args)
+// FilterService provides methods for managing filters.
+type FilterService struct {
+	client *Client
 }
 
-func (c *Client) UpdateFilter(ctx context.Context, args *sync.FilterUpdateArgs) (*sync.SyncResponse, error) {
-	return c.ExecuteCommand(ctx, args)
+func NewFilterService(client *Client) *FilterService {
+	return &FilterService{client: client}
 }
 
-func (c *Client) DeleteFilter(ctx context.Context, args *sync.FilterDeleteArgs) (*sync.SyncResponse, error) {
-	return c.ExecuteCommand(ctx, args)
+func (s *FilterService) AddFilter(ctx context.Context, args *sync.FilterAddArgs) (*sync.SyncResponse, error) {
+	return s.client.ExecuteCommand(ctx, args)
 }
 
-func (c *Client) DeleteFilters(ctx context.Context, args []*sync.FilterDeleteArgs) (*sync.SyncResponse, error) {
-	return c.ExecuteCommands(ctx, sync.NewCommands(args))
+func (s *FilterService) UpdateFilter(ctx context.Context, args *sync.FilterUpdateArgs) (*sync.SyncResponse, error) {
+	return s.client.ExecuteCommand(ctx, args)
 }
 
-func (c *Client) ReorderFilters(ctx context.Context, args *sync.FilterReorderArgs) (*sync.SyncResponse, error) {
-	return c.ExecuteCommand(ctx, args)
+func (s *FilterService) ReorderFilters(ctx context.Context, args *sync.FilterReorderArgs) (*sync.SyncResponse, error) {
+	return s.client.ExecuteCommand(ctx, args)
+}
+
+func (s *FilterService) DeleteFilter(ctx context.Context, args *sync.FilterDeleteArgs) (*sync.SyncResponse, error) {
+	return s.client.ExecuteCommand(ctx, args)
+}
+
+func (s *FilterService) DeleteFilters(ctx context.Context, args []*sync.FilterDeleteArgs) (*sync.SyncResponse, error) {
+	return s.client.ExecuteCommands(ctx, sync.NewCommands(args))
 }
