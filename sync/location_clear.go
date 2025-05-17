49398f495d5e220c8ca44dff8f0871eca37afc59
CnTeng <rxsnakepi@gmail.com>
2025-05-17 23:03:00 +0800

    refactor(location): add LocationService for location api

--- sync/location_clear.go
+++ sync/location_clear.go
@@ -1,10 +1,5 @@
 package sync
 
-// Clears the locations list, which is used for location reminders.
-//
-// See [Clear locations] for more details.
-//
-// [Clear locations]: https://todoist.com/api/v1/docs#tag/Sync/Reminders/Locations
 type LocationClearArgs struct{}
 
 func (args *LocationClearArgs) command() string {
--- todoist/location.go
+++ todoist/location.go
@@ -6,6 +6,20 @@ import (
 	"github.com/CnTeng/todoist-api-go/sync"
 )
 
-func (c *Client) ClearLocations(ctx context.Context) (*sync.SyncResponse, error) {
-	return c.ExecuteCommand(ctx, &sync.LocationClearArgs{})
+// LocationService provides methods for managing locations.
+type LocationService struct {
+	client *Client
+}
+
+func NewLocationService(client *Client) *LocationService {
+	return &LocationService{client: client}
+}
+
+// Clears the locations list, which is used for location reminders.
+//
+// See [Clear locations] for more details.
+//
+// [Clear locations]: https://todoist.com/api/v1/docs#tag/Sync/Reminders/Locations
+func (s *LocationService) ClearLocations(ctx context.Context) (*sync.SyncResponse, error) {
+	return s.client.ExecuteCommand(ctx, &sync.LocationClearArgs{})
 }
